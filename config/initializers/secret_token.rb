9d251ae0aaf93eef85c349c03ff8b532d022fc82
Ronald Velasquez <ronaldvg93@gmail.com>
2014-04-05 22:46:27 -0500

    Creando aplicacion para backups

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pgbackups::Application.config.secret_key_base = '2a11fcdcbdcbcf9c2f1e6d8ad7762b62827b0199e435594d83f01d523e87994bf5dcbc13709603dc20db024fa4945ecc118a77ab1022727812b44ebf48be4086'
